6efca918ff38d58926d422d52dd2d0233d68025f
VadimN1971 <nastenko1971@yandex.ru>
2025-05-12 00:10:55 +0700

    Правильно

---
+++ src/javarush_Vad/level_6/lesson_1/task_2/L6L1T2_ok.java
@@ -0,0 +1,30 @@
+package javarush_Vad.level_6.lesson_1.task_2;
+
+/*
+Четные и нечетные ячейки массива
+*/
+
+public class L6L1T2_ok {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[5];
+
+    public static void main(String[] args) {
+        strings[0] = EVEN;
+        strings[1] = ODD;
+        strings[2] = EVEN;
+        strings[3] = ODD;
+        strings[4] = EVEN;
+
+        System.out.print("index = 0");
+        System.out.println(" value = " + strings[0]);
+        System.out.print("index = 1");
+        System.out.println(" value = " + strings[1]);
+        System.out.print("index = 2");
+        System.out.println(" value = " + strings[2]);
+        System.out.print("index = 3");
+        System.out.println(" value = " + strings[3]);
+        System.out.print("index = 4");
+        System.out.println(" value = " + strings[4]);
+    }
+}
