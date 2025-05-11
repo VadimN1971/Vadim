c7b5aacd4e8e2e1c542ef3cf950ae3d8bd92f367
VadimN1971 <nastenko1971@yandex.ru>
2025-05-12 00:27:56 +0700

    Условие

---
+++ src/javarush_Vad/level_6/lesson_1/task_3/L6L1T3.java
@@ -0,0 +1,20 @@
+package javarush_Vad.level_6.lesson_1.task_3;
+
+/*
+Работаем с элементами массива
+*/
+
+public class L6L1T3 {
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+    public static void main(String[] args) {
+
+
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+
+}
