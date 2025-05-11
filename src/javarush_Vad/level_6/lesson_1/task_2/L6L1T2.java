60527667abd214111fd4e5198ed9b3a3249765e3
VadimN1971 <nastenko1971@yandex.ru>
2025-05-11 23:38:12 +0700

    Правильно

--- src/javarush_Vad/level_6/lesson_1/task_2/L6L1T2.java
+++ src/javarush_Vad/level_6/lesson_1/task_2/L6L1T2.java
@@ -12,30 +12,28 @@ public class L6L1T2 {
 
     public static void main(String[] args) {
 
-        for (int index = 0; index <= 5; index++) {
-            int strings;
-            for (int i = 0; i <= 5; i++) {
-                if (i % 2 == 0) {
-                    continue;
-                } else if {
-                    strings = String EVEN;
-                } else {
-                    strings = (ODD);
-                }
+
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                strings[i] = EVEN;
+            } else {
+                strings[i] = ODD;;
+
 
 
                 System.out.print("index = 0");
-                System.out.println(" value = " + strings[0]);
+                System.out.println(" value = " + strings[0]); //"index = 0 value = EVEN
                 System.out.print("index = 1");
-                System.out.println(" value = " + strings[1]);
-                System.out.print("index = 2");
-                System.out.println(" value = " + strings[2]);
-                System.out.print("index = 3");
-                System.out.println(" value = " + strings[3]);
-                System.out.print("index = 4");
-                System.out.println(" value = " + strings[4]);
+                System.out.println(" value = " + strings[1]); //"index = 1 value = ODD
+                //System.out.print("index = 2");
+                //System.out.println(" value = " + strings[2]); //"index = 2 value = EVEN
+                //System.out.print("index = 3");
+                //System.out.println(" value = " + strings[3]); //"index = 1 value = ODD
+                //System.out.print("index = 4");
+                //System.out.println(" value = " + strings[4]); //"index = 4 value = EVEN
             }
         }
+
     }
 }
 
