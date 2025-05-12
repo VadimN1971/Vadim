f190392bebbb72f06cec97861e3afc6848b360c2
VadimN1971 <nastenko1971@yandex.ru>
2025-05-12 22:35:52 +0700

    Решено
    Объединяем массивы

---
+++ src/javarush_Vad/level_6/lesson_2/task_1/L6L2T1.java
@@ -0,0 +1,19 @@
+package javarush_Vad.level_6.lesson_2.task_1;
+
+/*
+Объединяем массивы
+*/
+
+public class L6L2T1 {
+
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + ", ");
+        }
+    }
+}
