688e40e70746fa952bcccc8bbd7be3c1b0577a70
VadimN1971 <nastenko1971@yandex.ru>
2025-05-13 16:56:55 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_3/task_2/L6L3T2.java
@@ -0,0 +1,17 @@
+package javarush_Vad.level_6.lesson_3.task_2;
+
+import java.util.Scanner;
+
+/*
+Минимальное из N чисел
+*/
+
+public class L6L3T2 {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+
+    }
+}
