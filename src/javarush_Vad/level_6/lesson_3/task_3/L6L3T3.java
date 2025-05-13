d1a4eac60a56376b6873a705c5eb27b733f5cc41
VadimN1971 <nastenko1971@yandex.ru>
2025-05-13 21:09:02 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_3/task_3/L6L3T3.java
@@ -0,0 +1,15 @@
+package javarush_Vad.level_6.lesson_3.task_3;
+
+import java.util.Scanner;
+
+/*
+Максимальное из N чисел
+*/
+
+public class L6L3T3 {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+    }
+}
