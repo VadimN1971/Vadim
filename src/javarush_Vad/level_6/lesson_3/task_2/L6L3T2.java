1f2be6032cde758a87ae4a2b41b65cf40571e0c0
VadimN1971 <nastenko1971@yandex.ru>
2025-05-13 21:03:52 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_6/lesson_3/task_2/L6L3T2.java
+++ src/javarush_Vad/level_6/lesson_3/task_2/L6L3T2.java
@@ -10,8 +10,22 @@ public class L6L3T2 {
     public static int[] array;
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int N = console.nextInt();
+        array = new int[N];
 
+        // for (int i = 0; i < N; i++) {
+        //    array[i] = console.nextInt();
+        // } int min = array[0];
+        // for (int i = 1; i < N; i++) {
 
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println(min);
     }
 }
