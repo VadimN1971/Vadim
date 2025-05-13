41b871f5b99084ec9524fd45694367760884d874
VadimN1971 <nastenko1971@yandex.ru>
2025-05-13 21:31:12 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_4/task_1/L6L4T1.java
@@ -0,0 +1,18 @@
+package javarush_Vad.level_6.lesson_4.task_1;
+
+import java.util.Scanner;
+
+/*
+Удаляем одинаковые строки
+*/
+
+public class L6L4T1 {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
