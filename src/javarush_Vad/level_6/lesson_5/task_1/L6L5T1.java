b7abb61ae24c9789f1cf461ca3d0b290a9152058
VadimN1971 <nastenko1971@yandex.ru>
2025-05-15 02:41:43 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_5/task_1/L6L5T1.java
@@ -0,0 +1,15 @@
+package javarush_Vad.level_6.lesson_5.task_1;
+
+/*
+Таблица умножения
+*/
+
+public class L6L5T1 {
+
+    public static int[][] MULTIPLICATION_TABLE;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}
