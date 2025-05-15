bcb7b9eeca851cc6ae3ad33ce89be7b956cd8522
VadimN1971 <nastenko1971@yandex.ru>
2025-05-15 23:06:49 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_6/task_1/L6L6T1.java
@@ -0,0 +1,13 @@
+package javarush_Vad.level_6.lesson_6.task_1;
+
+/*
+Треугольный массив
+ */
+
+public class L6L6T1 {
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
