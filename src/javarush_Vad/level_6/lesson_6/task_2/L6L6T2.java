6621aa2bd6a6592dd2fc98849ca183de1a3eeff0
VadimN1971 <nastenko1971@yandex.ru>
2025-05-16 01:28:29 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_6/task_2/L6L6T2.java
@@ -0,0 +1,15 @@
+package javarush_Vad.level_6.lesson_6.task_2;
+
+import java.util.Scanner;
+
+/*
+Создаем двумерный массив
+*/
+
+public class L6L6T2 {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+}
