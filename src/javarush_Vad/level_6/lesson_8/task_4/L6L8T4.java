78ffcd9175da07a0f226bba20ef03b3e9fa13b21
VadimN1971 <nastenko1971@yandex.ru>
2025-05-18 00:48:18 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_8/task_4/L6L8T4.java
@@ -0,0 +1,18 @@
+package javarush_Vad.level_6.lesson_8.task_4;
+
+import java.util.Arrays;
+
+/*
+Есть ли кто?
+*/
+
+public class L6L8T4 {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    public static int element = 5;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}
