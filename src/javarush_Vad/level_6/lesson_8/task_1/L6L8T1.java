9c39f27ac82177b8f1ef2baf9ba6638a56d8a8a7
VadimN1971 <nastenko1971@yandex.ru>
2025-05-17 17:40:47 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_6/lesson_8/task_1/L6L8T1.java
@@ -0,0 +1,23 @@
+package javarush_Vad.level_6.lesson_8.task_1;
+
+import java.util.Arrays;
+
+/*
+Заполняем массив
+*/
+
+public class L6L8T1 {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+
+
+
+        System.out.println(Arrays.toString(array));
+    }
+}
