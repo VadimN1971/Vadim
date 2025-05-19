beadc96f69a23f14c9f698a35ac2d4d764b06c30
VadimN1971 <nastenko1971@yandex.ru>
2025-05-19 23:00:05 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_2/task_4/L7L2T4.java
@@ -0,0 +1,42 @@
+package javarush_Vad.level_7.lesson_2.task_4;
+
+/*
+Универсальный солдат
+*/
+
+public class L7L2T4 {
+
+    public static void main(String[] args) {
+    }
+
+    public static void universalMethod(int[] data, int value) {
+    }
+
+    public static void universalMethod(String str) {
+    }
+
+    public static void universalMethod(int x) {
+    }
+
+    public static void universalMethod(int x, int y) {
+    }
+
+    public static void universalMethod(double val) {
+    }
+
+    public static void universalMethod(int[] assay) {
+    }
+
+    public static void universalMethod(int[][] data, int value) {
+    }
+
+    public static void universalMethod(String name, String name2) {
+    }
+
+    public static void universalMethod(int[][][] data, int value) {
+    }
+
+    public static void universalMethod() {
+    }
+
+}
