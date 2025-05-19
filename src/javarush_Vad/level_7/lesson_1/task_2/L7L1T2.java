2789c683fc5587cfdd1fddda19dc14f4a565d542
VadimN1971 <nastenko1971@yandex.ru>
2025-05-19 21:33:45 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_1/task_2/L7L1T2.java
@@ -0,0 +1,21 @@
+package javarush_Vad.level_7.lesson_1.task_2;
+
+/*
+Упорядоченная информация
+*/
+
+public class L7L1T2 {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        //print1();
+        //print2();
+        //}
+        //public static void print1() {
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        //}
+        //public static void print2(){
+        System.out.println("От меня вам всем приветик.");
+    }
+}
