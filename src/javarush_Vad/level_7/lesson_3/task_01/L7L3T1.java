f86720713e6ed5f0010d1fe59925ed5ccf0b01eb
VadimN1971 <nastenko1971@yandex.ru>
2025-05-20 19:31:40 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_7/lesson_3/task_01/L7L3T1.java
+++ src/javarush_Vad/level_7/lesson_3/task_01/L7L3T1.java
@@ -4,9 +4,6 @@ package javarush_Vad.level_7.lesson_3.task_01;
 Добро пожаловать! Но не всем.
 */
 
-import javax.swing.*;
-import java.util.Scanner;
-
 public class L7L3T1 {
     public static void main(String[] args) {
     }
--- src/javarush_Vad/level_7/lesson_3/task_02/L7L3T2.java
+++ src/javarush_Vad/level_7/lesson_3/task_02/L7L3T2.java
@@ -8,8 +8,7 @@ public class L7L3T2 {
     public static void main(String[] args) {
     }
 
-
-
-
-
+    public static long cube(long a) {
+        return a * a * a;
+    }
 }
