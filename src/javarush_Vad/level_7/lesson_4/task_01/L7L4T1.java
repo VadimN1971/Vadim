ad599db66658b99994684bf9e63d90929bdfa86c
VadimN1971 <nastenko1971@yandex.ru>
2025-05-21 00:21:05 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_4/task_01/L7L4T1.java
@@ -0,0 +1,17 @@
+package javarush_Vad.level_7.lesson_4.task_01;
+
+/*
+Борьба за доступ
+*/
+
+public class L7L4T1 {
+
+    public static void main(String[] args) {
+        Person person = new person("Иван", "Иванов");
+        System.out.println("Досье.");
+        System.out.println("Имя: " + person.getFirstName());
+        System.out.println("Фамилия: " + person.getLastName());
+        System.out.println("Полное имя: " + person.getFullName());
+    }
+}
+
--- src/javarush_Vad/temp/t04/Test04.java
+++ src/javarush_Vad/temp/t04/Test04.java
@@ -1,27 +1,12 @@
 package javarush_Vad.temp.t04;
 
-import java.util.Scanner;
-/*
-Таблица умножения > умножение
-*/
-
 public class Test04 {
-    public static int[][] multiArray;
+
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        multiArray = new int[N][];
-        for (int i = 0; i < N; i++) {
-            int size = scanner.nextInt();
-            multiArray[i] = new int[size];
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < multiArray[i].length; j++) {
-                System.out.print(multiArray[i][j] + " ");
-            }
-            System.out.println();
-        }
-        scanner.close();
+
+
+
+
     }
 }
