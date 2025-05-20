2963ed600a06b4696d1a69e3c4cebb139afda1bc
VadimN1971 <nastenko1971@yandex.ru>
2025-05-20 19:34:34 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_3/task_03/L7L3T3.java
@@ -0,0 +1,16 @@
+package javarush_Vad.level_7.lesson_3.task_03;
+
+/*
+Кубический калькулятор в кубе
+*/
+
+public class L7L3T3 {
+    public static void main(String[] args) {
+    }
+
+    public static long cube(long a) {
+        return a * a * a;
+    }
+    //напишите тут ваш код
+}
+
