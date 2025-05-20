1585bd77ee01d84db39a494ae241baa07490e000
VadimN1971 <nastenko1971@yandex.ru>
2025-05-21 03:10:08 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_7/lesson_4/task_01/L7L4T1.java
+++ src/javarush_Vad/level_7/lesson_4/task_01/L7L4T1.java
@@ -7,11 +7,11 @@ package javarush_Vad.level_7.lesson_4.task_01;
 public class L7L4T1 {
 
     public static void main(String[] args) {
-        Person person = new person("Иван", "Иванов");
+        //Person person = new Person ("Иван", "Иванов");
         System.out.println("Досье.");
-        System.out.println("Имя: " + person.getFirstName());
-        System.out.println("Фамилия: " + person.getLastName());
-        System.out.println("Полное имя: " + person.getFullName());
+        //System.out.println("Имя: " + person.getFirstName());
+        //System.out.println("Фамилия: " + person.getLastName());
+        //System.out.println("Полное имя: " + person.getFullName());
     }
 }
 
--- src/javarush_Vad/temp/t04/Test04.java
+++ src/javarush_Vad/temp/t04/Test04.java
@@ -4,9 +4,22 @@ public class Test04 {
 
 
     public static void main(String[] args) {
+        user2(); //в методе main обязательна передача работе программе следующему методу!
+    }
 
+    public static void user1() {
+        System.out.println("user1");
+        user3(); //метод не запустится, так как user2 от main никакому другому методу не был передан.
+        user1(); //А метод 3 тоже никем не был запущен, так как за границей метода user2 и там остановился без передачи дальше.
+    }
 
+    public static void user2() {
+        System.out.println("user2");
+    }
 
-
+    public static void user3() {
+        System.out.println("user3");
+        //user2();
+        //user1();
     }
 }
