bb54ae793c1b7246ce64fd8ee4781e7e274cefa4
VadimN1971 <nastenko1971@yandex.ru>
2025-05-22 14:02:28 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_7/lesson_4/task_01/L7L4T1.java
+++ src/javarush_Vad/level_7/lesson_4/task_01/L7L4T1.java
@@ -3,18 +3,18 @@ package javarush_Vad.level_7.lesson_4.task_01;
 /*
 Борьба за доступ
 */
-
+//под слэшами решение для сайта JavaRush
 public class L7L4T1 {
-
     public static void main(String[] args) {
         Person person = new Person("Иван", "Иванов");
         System.out.println("Досье.");
         System.out.println("Имя: " + person.getFirstName());
         System.out.println("Фамилия: " + person.getLastName());
         System.out.println("Полное имя: " + person.getFullName());
-
     }
 }
+
+//public class Person {
 class Person {
     private String firstName;
     private String lastName;
@@ -24,14 +24,20 @@ class Person {
         this.lastName = lastName;
     }
 
+    //protected String getFirstName() {
+    //        return firstName;
     public String getFirstName() {
         return firstName;
     }
 
+    //String getLastName() {
+    //    return lastName;
     public String getLastName() {
         return lastName;
     }
 
+    //public String getFullName() {
+    //        return firstName + " " + lastName;
     public String getFullName() {
         return firstName + " " + lastName;
     }
