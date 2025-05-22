0543e618920f4689d50aa94cfe7667932f9846f5
VadimN1971 <nastenko1971@yandex.ru>
2025-05-23 02:06:29 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_6/task_1/L7L6T1.java
@@ -0,0 +1,17 @@
+package javarush_Vad.level_7.lesson_6.task_1;
+
+/*
+Вспоминаем геометрию
+*/
+
+public class L7L6T1 {
+
+    public static final double PI = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Площадь круга равна: " + PI * radius * radius);
+        System.out.println("Периметр круга равен: " + PI * diameter);
+    }
+}
