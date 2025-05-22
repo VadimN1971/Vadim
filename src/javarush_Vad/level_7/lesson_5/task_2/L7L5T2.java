502ce3b758de285ceec9d9b45b3d24d8e39d6a36
VadimN1971 <nastenko1971@yandex.ru>
2025-05-23 01:58:29 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_7/lesson_5/task_2/L7L5T2.java
+++ src/javarush_Vad/level_7/lesson_5/task_2/L7L5T2.java
@@ -6,7 +6,7 @@ package javarush_Vad.level_7.lesson_5.task_2;
 
 public class L7L5T2 {
 
-    public static String city = "Токио";
+    public static String City = "Токио";
     public static double population = 34.5;
 
     public static void main(String[] args) {
@@ -16,8 +16,10 @@ public class L7L5T2 {
         printCityPopulation("Нью-Йорк", 21.6);
     }
 
-    public static void printCityPopulation(String city, double population){
+    public static void printCityPopulation(String city, double population) {
         System.out.println("Население города " + city + " составляет " + population + " млн человек.");
-        System.out.println("В то время как в самом густонаселенном городе " + city + " население составляет " + population + " млн человек.");
+        System.out.println("В то время как в самом густонаселенном городе " + L7L5T2.City + " население составляет " + L7L5T2.population + " млн человек.");
     }
 }
+
+
--- src/javarush_Vad/temp/t06/Test06.java
+++ src/javarush_Vad/temp/t06/Test06.java
@@ -1,3 +1,5 @@
 package javarush_Vad.temp.t06;
 
 public class Test06 {
+
+}
