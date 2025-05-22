0d2845bf3a3784d212cef75ab1fb0b12a9981697
VadimN1971 <nastenko1971@yandex.ru>
2025-05-23 00:49:59 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_5/task_2/L7L5T2.java
@@ -0,0 +1,23 @@
+package javarush_Vad.level_7.lesson_5.task_2;
+
+/*
+Города-гиганты
+*/
+
+public class L7L5T2 {
+
+    public static String city = "Токио";
+    public static double population = 34.5;
+
+    public static void main(String[] args) {
+        printCityPopulation("Джакарта", 25.3);
+        printCityPopulation("Сеул", 25.2);
+        printCityPopulation("Дели", 23.1);
+        printCityPopulation("Нью-Йорк", 21.6);
+    }
+
+    public static void printCityPopulation(String city, double population){
+        System.out.println("Население города " + city + " составляет " + population + " млн человек.");
+        System.out.println("В то время как в самом густонаселенном городе " + city + " население составляет " + population + " млн человек.");
+    }
+}
---
+++ src/javarush_Vad/temp/t06/Test06.java
@@ -0,0 +1,3 @@
+package javarush_Vad.temp.t06;
+
+public class Test06 {
