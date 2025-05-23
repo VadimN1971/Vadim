4cb378e84d8bbbb8419ea68510f496061978a772
VadimN1971 <nastenko1971@yandex.ru>
2025-05-23 17:26:16 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_7/lesson_6/task_2/L7L6T2.java
@@ -0,0 +1,15 @@
+package javarush_Vad.level_7.lesson_6.task_2;
+
+/*
+Все что нужно знать о Земле
+*/
+
+public class L7L6T2 {
+
+    public class Earth {
+        String name = "Земля";
+        double square = 510_100_000;
+        long population = 7_594_000_000L;
+        long equatorLength = 40_075_696;
+    }
+}
