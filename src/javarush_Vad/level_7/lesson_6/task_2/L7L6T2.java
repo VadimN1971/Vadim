8d2a5df162a6aaada8edbd04e1a59ecb1797cea9
VadimN1971 <nastenko1971@yandex.ru>
2025-05-23 17:41:07 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_7/lesson_6/task_2/L7L6T2.java
+++ src/javarush_Vad/level_7/lesson_6/task_2/L7L6T2.java
@@ -6,10 +6,10 @@ package javarush_Vad.level_7.lesson_6.task_2;
 
 public class L7L6T2 {
 
-    public class Earth {
-        String name = "Земля";
-        double square = 510_100_000;
-        long population = 7_594_000_000L;
-        long equatorLength = 40_075_696;
+    //public class Earth {
+        public static final String NAME = "Земля";
+        public static final double SQUARE = 510_100_000;
+        public static final long POPULATION = 7_594_000_000L;
+        public static final long EQUATOR_LENGTH = 40_075_696;
     }
-}
+
---
+++ src/javarush_Vad/level_7/lesson_6/task_3/L7L6T3.java
@@ -0,0 +1,20 @@
+package javarush_Vad.level_7.lesson_6.task_3;
+
+/*
+Учет работников
+*/
+
+public class L7L6T3 {
+
+    public String name = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
+    public void setPosition(String position) {
+        position = position;
+    }
+
+    public void setSalary(int salary) {
+        salary = salary;
+    }
+}
