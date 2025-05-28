fd621954ad4082975507cc9372a9b810e4536a83
VadimN1971 <nastenko1971@yandex.ru>
2025-05-28 23:56:34 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_8/lesson_1/task_4/L8L1T4.java
@@ -0,0 +1,13 @@
+package javarush_Vad.level_8.lesson_1.task_4;
+
+/*
+Экспонентная запись числа
+*/
+
+public class L8L1T4 {
+    double earthDiameter = 1.2742E+4;
+    double lightSpeed = 2.99792458E+8;
+    double uraniumAtomicMass = 2.380289e+2;
+    double averageBeeWeight = 8.5e-2;
+    double javaDeveloperSalary = 1e+4;
+}
---
+++ src/javarush_Vad/level_8/lesson_1/task_5/L8L1T5.java
@@ -0,0 +1,15 @@
+package javarush_Vad.level_8.lesson_1.task_5;
+
+/*
+Бесконечность не предел
+*/
+
+public class L8L1T5 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
