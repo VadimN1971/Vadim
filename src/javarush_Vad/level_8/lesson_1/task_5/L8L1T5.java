5b1d2177eae1f64b9475c991526c066be47a02cd
VadimN1971 <nastenko1971@yandex.ru>
2025-05-29 00:38:24 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_8/lesson_1/task_5/L8L1T5.java
+++ src/javarush_Vad/level_8/lesson_1/task_5/L8L1T5.java
@@ -7,9 +7,11 @@ package javarush_Vad.level_8.lesson_1.task_5;
 public class L8L1T5 {
     public static void main(String[] args) {
         //напишите тут ваш код
+        div(0.0, 100.0);
+        div(0.0, -100.0);
     }
+    public static void div(double a, double b) {
 
-    public static void div(double a, double b){
-        System.out.println(b/a);
+        System.out.println(b / a);
     }
 }
---
+++ src/javarush_Vad/level_8/lesson_1/task_6/L8L1T6.java
@@ -0,0 +1,17 @@
+package javarush_Vad.level_8.lesson_1.task_6;
+
+/*
+Странное деление
+*/
+
+public class L8L1T6 {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        div(0.0,0.0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
