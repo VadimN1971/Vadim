0b5fe3f0b9a33be8ada2db61ed56b3280dd915ac
VadimN1971 <nastenko1971@yandex.ru>
2025-05-31 02:02:40 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_8/lesson_4/task_1/L8L4T1.java
@@ -0,0 +1,15 @@
+package javarush_Vad.level_8.lesson_4.task_1;
+
+/*
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class L8L4T1 {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+    }
+
+}
