3ca2a2af508054de318aeeb3e2a16fd7aee3b666
VadimN1971 <nastenko1971@yandex.ru>
2025-05-30 15:02:50 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_8/lesson_2/task_2/L8L2T2.java
@@ -0,0 +1,18 @@
+package javarush_Vad.level_8.lesson_2.task_2;
+
+/*
+Очень правильные типы
+*/
+
+public class L8L2T2 {
+
+    long a = 109 + 15;
+    int b = (int) (a * 2);
+    short c = (short) (a / b);
+    byte d = (byte) (a + b - c);
+
+    //long a = 109 + 15;
+    //int b = a * 2;
+    //short c = a / b;
+    //byte d = a + b - c;
+}
