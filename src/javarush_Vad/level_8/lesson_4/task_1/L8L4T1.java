bb93d853728b06e70b188e1ee6ed40b38aa5cfa2
VadimN1971 <nastenko1971@yandex.ru>
2025-05-31 10:40:58 +0700

    Решено
    Reverse

--- src/javarush_Vad/level_8/lesson_4/task_1/L8L4T1.java
+++ src/javarush_Vad/level_8/lesson_4/task_1/L8L4T1.java
@@ -10,9 +10,24 @@ public class L8L4T1 {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        String a = new String("Привет");
+        String b = new String("Привет");
+        String c = new String("Привет");
+        String d = new String("Привет");
+        String e = new String("Привет");
+        String f = new String("Привет");
+        String g = new String("Привет");
+        String h = new String("Привет");
+        String i = new String("Привет");
+        String j = new String("Привет");
 
+        int[] k = new int[10];
+        int[] l = new int[10];
+        int[] m = new int[10];
+        int[] n = new int[10];
+        int[] o = new int[10];
 
-
-
+        Scanner console = new Scanner("");
+        Scanner str = new Scanner("");
     }
 }
