11506bfb36ec72ac945df201aa2a770f963d435a
VadimN1971 <nastenko1971@yandex.ru>
2025-06-05 16:43:39 +0700

    Решено
    Reverse

--- src/javarush_Vad/temp/t06/Test06.java
+++ src/javarush_Vad/temp/t06/Test06.java
@@ -1,5 +1,11 @@
 package javarush_Vad.temp.t06;
 
+import java.util.Scanner;
+
 public class Test06 {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+    }
 
 }
