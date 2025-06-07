244c9a6407dac4ca6da43abbea887614083e659a
VadimN1971 <nastenko1971@yandex.ru>
2025-06-07 15:15:13 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_8/lesson_7/task_2/L8L7T2.java
@@ -0,0 +1,46 @@
+package javarush_Vad.level_8.lesson_7.task_2;
+
+public class L8L7T2 {
+
+    public class ElectricCar {
+        //double maxSpeed;
+        //int wheelCount;
+        //double weight;
+        int electricEnginePower;
+
+        class Vehicle {
+            double maxSpeed;
+
+            class Car {
+                //double maxSpeed;
+                int wheelCount;
+                double weight;
+            }
+        }
+    }
+}
+
+/*
+***ElectricCar.java
+
+public class ElectricCar {
+    //double maxSpeed;
+    //int wheelCount;
+    //double weight;
+    int electricEnginePower;
+}
+
+***Vehicle.java
+
+public class Vehicle {
+    double maxSpeed;
+}
+
+***Car.java
+
+public class Car {
+    //double maxSpeed;
+    int wheelCount;
+    double weight;
+}
+ */
