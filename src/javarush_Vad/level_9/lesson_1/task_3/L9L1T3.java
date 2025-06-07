fb5e6bed1cc0d700c047689154dd2561c48ae6d3
VadimN1971 <nastenko1971@yandex.ru>
2025-06-07 23:56:43 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_9/lesson_1/task_3/L9L1T3.java
@@ -0,0 +1,25 @@
+package javarush_Vad.level_9.lesson_1.task_3;
+
+import java.util.Scanner;
+
+/*
+Минимальный элемент массива
+*/
+
+public class L9L1T3 {
+
+    public static void main(String[] args) {
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] ints) {
+        //напишите тут ваш код
+        return 0;
+    }
+
+    public static int[] getArrayOfTenElements() {
+        //напишите тут ваш код
+        return new int[10];
+    }
+}
