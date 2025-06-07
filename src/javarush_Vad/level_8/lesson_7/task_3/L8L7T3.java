9b08582d19b3fed399fcdefc50ae18767e2b965a
VadimN1971 <nastenko1971@yandex.ru>
2025-06-07 15:23:20 +0700

    Решено
    Reverse

---
+++ src/javarush_Vad/level_8/lesson_7/task_3/L8L7T3.java
@@ -0,0 +1,54 @@
+package javarush_Vad.level_8.lesson_7.task_3;
+
+public class L8L7T3 {
+}
+
+/*
+***Entity.java
+
+public class Entity {
+    public void move(){
+        System.out.println("Я передвигаюсь.");
+    }
+
+    public void eat(){
+        System.out.println("Я ем.");
+    }
+}
+
+***Human.java
+
+public class Human {
+    //public void move(){
+    //    System.out.println("Я передвигаюсь.");
+    //}
+
+    //public void eat(){
+    //    System.out.println("Я ем.");
+    //}
+
+    public void speak(){
+        System.out.println("Я умею общаться.");
+    }
+}
+
+***JavaDeveloper.java
+
+public class JavaDeveloper {
+    //public void move(){
+    //    System.out.println("Я передвигаюсь.");
+    //}
+
+    //public void eat(){
+    //    System.out.println("Я ем.");
+    //}
+
+    //public void speak(){
+    //    System.out.println("Я умею общаться.");
+    //}
+
+    public void code(){
+        System.out.println("Я умею общаться на Java.");
+    }
+}
+ */
