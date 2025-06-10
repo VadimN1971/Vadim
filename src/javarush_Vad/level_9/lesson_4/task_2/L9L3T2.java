94c6ffd58fc29e2cc3d376d3c7a6ff826d3892ad
VadimN1971 <nastenko1971@yandex.ru>
2025-06-11 00:52:28 +0700

    Решено

--- src/javarush_Vad/level_9/lesson_4/task_2/L9L3T2.java
+++ src/javarush_Vad/level_9/lesson_4/task_2/L9L3T2.java
@@ -1,4 +1,33 @@
 package javarush_Vad.level_9.lesson_4.task_2;
 
+/*
+Counter
+*/
+
 public class L9L3T2 {
+    public static void main(String[] args) throws InterruptedException {
+        printNumbers();
+    }
+
+    public static void printNumbers() throws InterruptedException {
+        System.out.println("1");
+        Thread.sleep(1, 300_000);
+        System.out.println("2");
+        Thread.sleep(1, 300_000);
+        System.out.println("3");
+        Thread.sleep(1, 300_000);
+        System.out.println("4");
+        Thread.sleep(1, 300_000);
+        System.out.println("5");
+        Thread.sleep(1, 300_000);
+        System.out.println("6");
+        Thread.sleep(1, 300_000);
+        System.out.println("7");
+        Thread.sleep(1, 300_000);
+        System.out.println("8");
+        Thread.sleep(1, 300_000);
+        System.out.println("9");
+        Thread.sleep(1, 300_000);
+        System.out.println("10");
+    }
 }
--- src/javarush_Vad/level_9/lesson_4/task_3/L9L3T3.java
+++ src/javarush_Vad/level_9/lesson_4/task_3/L9L3T3.java
@@ -1,4 +1,24 @@
 package javarush_Vad.level_9.lesson_4.task_3;
 
+import java.util.concurrent.TimeUnit;
+
+/*
+Таймер
+*/
+
 public class L9L3T3 {
+
+    public static void setTimer(int days, int hours, int minutes, int seconds, int millis) throws InterruptedException {
+        System.out.println("Таймер запущен!");
+        TimeUnit.DAYS.sleep(days);
+        TimeUnit.HOURS.sleep(hours);
+        TimeUnit.MINUTES.sleep(minutes);
+        TimeUnit.SECONDS.sleep(seconds);
+        TimeUnit.MILLISECONDS.sleep(millis);
+        System.out.println("♬ ♪ ♬♬♬♬ ♪♪♪♪");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        setTimer(0, 0, 0, 5, 5);
+    }
 }
