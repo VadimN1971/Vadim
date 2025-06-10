b7760d64461650815c0e9e393f7015bb97969eec
VadimN1971 <nastenko1971@yandex.ru>
2025-06-10 23:37:22 +0700

    Решено

--- src/javarush_Vad/level_9/lesson_3/task_1/L9L3T1.java
+++ src/javarush_Vad/level_9/lesson_3/task_1/L9L3T1.java
@@ -1,19 +1,22 @@
 package javarush_Vad.level_9.lesson_3.task_1;
 
-import java.io.BufferedReader;
-import java.io.StringReader;
-import java.math.BigInteger;
-import java.util.ArrayList;
+//import java.io.BufferedReader;
+//import java.io.StringReader;
+//import java.math.BigInteger;
+//import java.util.ArrayList;
 
 /*
 Импорты: Part 1
 */
 
 public class L9L3T1 {
-
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(new StringReader(""));
-        BigInteger bigInteger = new BigInteger("123");
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+
+        //ArrayList<String> arrayList = new ArrayList<>();
+        //BufferedReader bufferedReader = new BufferedReader(new StringReader(""));
+        //BigInteger bigInteger = new BigInteger("123");
     }
 }
--- src/javarush_Vad/level_9/lesson_3/task_2/L9L3T2.java
+++ src/javarush_Vad/level_9/lesson_3/task_2/L9L3T2.java
@@ -1,4 +1,17 @@
 package javarush_Vad.level_9.lesson_3.task_2;
 
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/*
+Импорты: Part 2
+*/
+
 public class L9L3T2 {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
 }
