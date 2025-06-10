302c6b3342b2dc57e87665eff25a51ea4240a8f7
VadimN1971 <nastenko1971@yandex.ru>
2025-06-11 00:03:27 +0700

    Решено

--- src/javarush_Vad/level_9/lesson_4/task_1/L9L3T1.java
+++ src/javarush_Vad/level_9/lesson_4/task_1/L9L3T1.java
@@ -1,4 +1,21 @@
 package javarush_Vad.level_9.lesson_4.task_1;
 
+/*
+Обратный отсчет
+*/
+
 public class L9L3T1 {
+    public static void main(String[] args) throws InterruptedException {
+        countDown();
+    }
+
+    public static void countDown() throws InterruptedException {
+        System.out.println("3");
+        Thread.sleep(1000);
+        System.out.println("2");
+        Thread.sleep(1000);
+        System.out.println("1");
+        Thread.sleep(1000);
+        System.out.println("Старт!");
+    }
 }
--- src/javarush_Vad/level_9/lesson_4/task_3/L9L3T3.java
+++ src/javarush_Vad/level_9/lesson_4/task_3/L9L3T3.java
@@ -1,21 +1,4 @@
 package javarush_Vad.level_9.lesson_4.task_3;
 
-/*
-Обратный отсчет
-*/
-
 public class L9L3T3 {
-    public static void main(String[] args) throws InterruptedException {
-        countDown();
-    }
-
-    public static void countDown() throws InterruptedException {
-        System.out.println("3");
-        Thread.sleep(1000);
-        System.out.println("2");
-        Thread.sleep(1000);
-        System.out.println("1");
-        Thread.sleep(1000);
-        System.out.println("Старт!");
-    }
 }
