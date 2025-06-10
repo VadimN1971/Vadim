b998d61ee852ff6acd5899c0acbe85c1553f28e7
VadimN1971 <nastenko1971@yandex.ru>
2025-06-10 22:18:04 +0700

    Решено
    Reverse

--- src/javarush_Vad/temp/t06/Test06.java
+++ src/javarush_Vad/temp/t06/Test06.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 
 public class Test06 {
 
-    public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-    }
+    public static String ups = "да";
+    public static String ops = "нет";
 
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("О прекрасная девица! Давайте займемся сексом?! )) \n Ответьте же скорее: Да или нет?");
+        String name = console.nextLine();
+        if (ups.equalsIgnoreCase(name))
+            System.out.println("УРА!!! В койку!!!");
+        else if (ops.equalsIgnoreCase(name)) {
+            System.out.println("Если девушка говорит нет, \n то она подразумевает ДА!!!");
+        } else System.out.println("Ответьте на вопрос точно: ДА или НЕТ! \n Для повторной попытки нажмите клавиши Ctrl+F10");
+    }
 }
