d40562cbd52f13a671450b516e83beae53ef11c5
VadimN1971 <nastenko1971@yandex.ru>
2025-06-16 01:10:58 +0700

    Решено

---
+++ src/javarush_Vad/temp/sortBubble03/sortBubble03.java
@@ -0,0 +1,24 @@
+package javarush_Vad.temp.sortBubble03;
+
+public class sortBubble03 {
+
+    public static int sort(int[] array) {
+        int count = 0;
+        int k = 1;
+        int j = array.length;
+        while (k > 0) {
+            k = 0;
+            for (int i = 1; i < j; i++) {
+                if (array[i - 1] > array[i]) {
+                    int tmp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = tmp;
+                    k = i;
+                }
+                count++;
+            }
+            j = k;
+        }
+        return count;
+    }
+}
